dc7d7a7ba72c622425653b55c6e1e3aa0f4fe1a9
GnanaGuru Mariyadass <Gnanaguru.Mariyadass@qfor.com>
2016-03-02 20:37:30 +0530

    Fixed.

--- wp-content/plugins/mod-flipper/mod-flipper.php
+++ wp-content/plugins/mod-flipper/mod-flipper.php
@@ -39,7 +39,7 @@ class Flipper {
     public function flipper_register_scripts_and_styles(){
         //Register scripts
         wp_register_script('mod-flipper',plugins_url( 'js/mod-flipper.js' , __FILE__ ),null,null,true);
-        wp_enqueue_script( 'jquery.flexslider-min',plugins_url( 'js/jquery.flexslider-min.js' , __FILE__ ),nu                
+        wp_enqueue_script( 'jquery.flexslider-min',plugins_url( 'js/jquery.flexslider-min.js' , __FILE__ ),null,null,true);          
     }
 
     /**
