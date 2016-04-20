befd9ac97501f2235b0413d127c17c664a564bd2
Boobalan Karuppusamy <boobalan.karuppusamy@qfor.com>
2016-04-20 18:41:41 +0530

    Adding custom search route

--- wp-content/themes/jeanknowscars/search.php
+++ wp-content/themes/jeanknowscars/search.php
@@ -18,8 +18,8 @@ get_header(); ?>
 
         <div class="mod-search">
             <div class="search-wrap">
-                  <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                        <input class="input-text" id='jkc_searchfield' type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Type your search here...', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'twentysixteen' ); ?>" />
+                  <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/search/' ) ); ?>">
+                        <input class="input-text" id='jkc_searchfield' type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Type your search here...', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="q" title="<?php echo esc_attr_x( 'Search for:', 'label', 'twentysixteen' ); ?>" />
                         <button class="search-button search-btn search-submit" tabindex="1" onclick="return checkmeNotEmpty();>
                             <i class="fa fa-search"></i>
                         </button>
