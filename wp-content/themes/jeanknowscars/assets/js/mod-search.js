90de7ff25309585943776b13bacf6a350de311f6
gnanaguru mariyadass <gnanaguru.mariyadass@qfor.com>
2016-04-21 15:26:36 +0530

    side bar sticky issue for search page and other pages

--- wp-content/themes/jeanknowscars/assets/js/mod-search.js
+++ wp-content/themes/jeanknowscars/assets/js/mod-search.js
@@ -2,71 +2,7 @@
 
 (function(sorcWeb, $){
 	"use strict";
-
-    sorcWeb.modSearch = {};
-
-	var mod = sorcWeb.modSearch;
-
-    sorcWeb.modSearch.init = function() {
-		mod.vars = {};
-		mod.setSelectors();
-		mod.setEvents();
-
-	};
-
-	mod.setSelectors = function() {
-		mod.module = $('.mod-search');
-		mod.searchBox = mod.module.find('.search-wrap');
-		mod.searchInput = mod.module.find('.input-text');
-	};
-
-	mod.setEvents = function(){
-		/*Trigger Search*/
-		mod.module.on('click', '.search-btn', function(e){
-			var valueStr = mod.searchInput.val().trim(),
-				value = encodeURIComponent(valueStr).toLowerCase();
-
-			if( value ){
-				e.preventDefault();
-				mod.doSearch(value);
-			}
-		});
-
-		mod.searchBox.keypress(function (e){
-			var code=(e.keyCode?e.keyCode:e.which),
-				valueStr = mod.searchInput.val().trim(),
-				value = encodeURIComponent(valueStr).toLowerCase();
-
-			if(code==13){
-				e.preventDefault();
-				if( value ){
-					mod.doSearch(value);
-				}
-			}
-		});
-
-		/*Trigger Search Results Page Tracking*/
-		$(document).on('click', '[data-searcharticlelink]', function(e) {
-			e.preventDefault();
-			//if (typeof omnitureTrackInner == 'function') {
-			//	var searchArticleUrl = $(this).attr('href');
-			//	var searchOrderNum = $(this).parents('[data-searchresultsorder]').data('searchresultsorder');
-             //   var valueStr = mod.searchInput.val().trim(),
-             //       value = encodeURIComponent(valueStr).toLowerCase();
-			//	omnitureTrackInner('oSearchClick', { "searchIndex": searchOrderNum }, $(this));
-			//}
-			window.location.href = searchArticleUrl;
-		});
-	};
-
-	mod.doSearch = function(value) {
-		var url = '/search/?q=' + value;
-		//if (typeof omnitureTrackInner == 'function') {
-		//	omnitureTrackInner('oSearch',{'search':value});
-		//}
-		window.location.href = url;
-	};
-
+        
     $(window).smartscroll(function(){
         var scrollY = $(document).scrollTop(),
             widthRightColumn = $('.right-column').width();
@@ -94,7 +30,5 @@
             }
         }
     });
-
-	return mod.init();
 
 })(SorcWeb, jQuery);
